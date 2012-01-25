d52a692f7443332c9ff9b3a7b2249c5c0340d608
Toxico Nimbus <toxn@free.fr>
2012-01-25 16:14:43 +0100

    Experimenting a new UI style, mimicking Google Reader's. Created a StyleSheet to easily change placements (just title bar for now). Created a Preferences class to centralize access to shared preferences, thus to improve detection of not localized hard coded strings.

---
+++ src/main/java/com/almarsoft/GroundhogReader/lib/Preferences.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+package com.almarsoft.GroundhogReader.lib;
+
+
+/**
+ * @author toxn
+ */
+@SuppressWarnings("nls")
+public final class Preferences
+{
+	public static final String  offlineMode         = "offlineMode";
+	public static final boolean offlineMode_Default = true;
+}
